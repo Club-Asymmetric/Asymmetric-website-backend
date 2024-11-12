4364514f2489a4f60091ceb926222c62d1fbf8fd
cosmic-striker <vishald.cs2023@citchennai.net>
2024-11-12 11:16:06 +0530

    file changes and data added

--- controllers/registrationController.js
+++
@@ -1,1 +0,0 @@
-export const registerUserHandler = (req, res) => { /* Implement logic */ };
--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -3,6 +3,19 @@ import * as eventController from '../controllers/eventController.js';
 import logger from '../middlewares/logger.js';
 const router = express.Router();
 
+
+import { getEvent, getAllEvents } from '../database/models.js'
+
+export const getAllEventsHandler = (req, res) => {
+    return res.json(getAllEvents());
+};
+export const getEventHandler = (req, res) => {
+    return res.json(getEvent(req.params.name));
+};
+
+
+
+
 router.use(logger('events'));
 router.get('/:name', eventController.getEventHandler);
 router.get('/', eventController.getAllEventsHandler);
