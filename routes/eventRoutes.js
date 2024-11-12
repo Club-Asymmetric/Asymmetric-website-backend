71ec3beb7ca0bc93405702acd6ca4886af2c5b31
JRBros2346 <jrbros2346@gmail.com>
2024-11-12 23:29:42 +0530

    [Events] Image from Database

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -1,8 +1,9 @@
-import express from "express";
+import express, { json } from "express";
 import logger from "../middlewares/logger.js";
 const router = express.Router();
 
 import { getEvent, getAllEvents } from "../models/event.js";
+import e from "express";
 
 router.use((req, res, next) => {
   logger.info(`${req.method} ${req.url}`);
@@ -12,7 +13,15 @@ router.get("/:name", async (req, res) => {
   return res.json(await getEvent(req.params.name));
 });
 router.get("/", async (req, res) => {
-  return res.json(await getAllEvents());
+  let events = await getAllEvents();
+  for (let i in events) {
+    for (let j in events[i].photos) {
+      let p = events[i].photos[j];
+      events[i].photos[j] = btoa(String.fromCharCode(...new Uint8Array(p)));
+    }
+  }
+  console.log(events);
+  return res.json(events);
 });
 
 export default router;
