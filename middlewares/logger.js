0587e21e3f38209ae13a30179350bd11f8a2628c
cosmic-striker <vishald.cs2023@citchennai.net>
2024-11-14 20:15:45 +0530

    dont know

--- middlewares/logger.js
+++ middlewares/logger.js
@@ -2,7 +2,7 @@ import winston from "winston";
 
 const logger = winston.createLogger({
   defaultMeta: { service: "user-service" },
-  level: "error",
+  level: "info",
   format: winston.format.combine(
     winston.format.timestamp(),
     winston.format.cli(),
@@ -10,16 +10,30 @@ const logger = winston.createLogger({
   ),
   transports: [
     new winston.transports.Console(),
-    new winston.transports.File({ filename: "logs/error.log" }),
+    new winston.transports.File({
+      filename: "logs/activity.log",
+      level: "info",
+    }),
+  ],
+  exceptionHandlers: [
+    new winston.transports.File({ filename: "logs/exceptions.log" }),
+  ],
+  rejectionHandlers: [
+    new winston.transports.File({ filename: "logs/rejections.log" }),
   ],
 });
 
 // Logging Middleware
 const logging = (req, res, next) => {
-  logger.info(`Incoming Request: ${req.method} ${req.url}`);
+  logger.info(`Incoming Request: ${req.method} ${req.url}`, {
+    headers: req.headers,
+    body: req.body,
+  });
 
   res.on("finish", () => {
-    logger.info(`Outgoing Response: ${res.statusCode}`);
+    logger.info(`Outgoing Response: ${res.statusCode}`, {
+      headers: res.getHeaders(),
+    });
   });
 
   next();
