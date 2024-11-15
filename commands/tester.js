e097300f2a453468cb8402584979f99a569bb788
JRBros2346 <jrbros2346@gmail.com>
2024-11-15 13:29:36 +0530

    [DATABASE] Updates 📦

--- commands/tester.js
+++ commands/tester.js
@@ -1,24 +1,44 @@
 import database from "../database.js";
 import { fileURLToPath } from "url";
 import path from "path";
-import fs from "fs";
+import fs from "fs/promises";
+import mime from "mime-types";
 import dotenv from "dotenv";
+import { publicDecrypt } from "crypto";
+import { RecordId } from "surrealdb";
 
 dotenv.config();
 
 const db = await database("event_registrations");
 try {
-  console.log(
-    await db.query(
-      fs.readFileSync(
-        path.join(
-          path.dirname(fileURLToPath(import.meta.url)),
-          "../migrations/test.surql"
-        ),
-        { encoding: "utf-8" }
-      )
-    )
+  let promises = [];
+  let pathStructure = {};
+  async function recursiveRead(dir, cur = pathStructure) {
+    const entries = await fs.readdir(dir, { withFileTypes: true });
+    for (const entry of entries) {
+      const fullPath = path.join(dir, entry.name);
+      if (entry.isDirectory()) {
+        cur[entry.name] = {};
+        await recursiveRead(fullPath, cur[entry.name]);
+      } else {
+        const mimeType = mime.lookup(entry.name);
+        if (mimeType && mimeType.startsWith("image/")) {
+          const photo = (
+            await db.create("photo", {
+              binary: await fs.readFile(fullPath, null),
+              mime: mimeType.split("/")[1],
+            })
+          )[0].id.id;
+          cur[entry.name] = photo;
+        }
+      }
+    }
+  }
+  await recursiveRead(
+    path.join(path.dirname(fileURLToPath(import.meta.url)), "../photos")
   );
+  console.log(pathStructure);
+  await Promise.all(promises);
 } catch (error) {
   console.error(
     "failed to migrate to surrealdb",
--- models/podcast.js
+++ models/podcast.js
@@ -2,11 +2,17 @@ import database from "../database.js";
 import { RecordId } from "surrealdb";
 
 export async function getPodcast(id) {
-  return await (await database()).select(new RecordId("podcast", id));
+  return await (
+    await database()
+  ).query("SELECT *, image.id() OMIT audio FROM $podcast", {
+    podcast: new RecordId("podcast", id),
+  });
 }
 
 export async function getAllPodcasts() {
-  return await (await database()).select("podcast");
+  return await (
+    await database()
+  ).query("SELECT *, image.id() OMIT audio FROM podcast");
 }
 
 export async function getPodcastAudio(id) {}
