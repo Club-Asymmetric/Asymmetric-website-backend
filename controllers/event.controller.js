6513323772bb636d76ea2df71975d718646c26d5
JRBros2346 <jrbros2346@gmail.com>
2024-11-16 15:14:16 +0530

    [FIXED] #36 [IMPROVED] #41

--- controllers/event.controller.js
+++ controllers/event.controller.js
@@ -4,6 +4,10 @@ export const getEvents = async (req, res) => {
   res.json(await getAllEvents());
 };
 
-export const getEventById = async (req, res) => {
-  res.json(await getEvent(req.params.id));
+export const getEventById = async (req, res, next) => {
+  try {
+    res.json(await getEvent(req.params.id));
+  } catch (error) {
+    next(error);
+  }
 };
--- controllers/photo.controller.js
+++ controllers/photo.controller.js
@@ -1,9 +1,12 @@
 import { ClientError } from "../errors/ApiError.js";
 import { getPhoto } from "../models/photo.js";
 
-export const getPhotoById = async (req, res) => {
-  let { binary, mime } = (await getPhoto(req.params.id)) ?? {};
-  if (!binary || !mime) throw ClientError.notFound();
-  res.header("Content-Type", `image/${mime}`);
-  res.send(Buffer.from(binary));
+export const getPhotoById = async (req, res, next) => {
+  try {
+    let { binary, mime } = (await getPhoto(req.params.id)) ?? {};
+    res.header("Content-Type", `image/${mime}`);
+    res.send(Buffer.from(binary));
+  } catch (error) {
+    next(error);
+  }
 };
