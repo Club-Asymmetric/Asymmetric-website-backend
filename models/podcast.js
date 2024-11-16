92bc800d1e07024d75fa65ee4977c8d64f6badc1
JRBros2346 <jrbros2346@gmail.com>
2024-11-16 14:48:51 +0530

    [FIXED] #32 and #33

--- models/podcast.js
+++ models/podcast.js
@@ -2,11 +2,15 @@ import database from "../database.js";
 import { RecordId } from "surrealdb";
 
 export async function getPodcast(id) {
-  return await (
-    await database()
-  ).query("SELECT *, image.id() OMIT audio FROM $podcast", {
-    podcast: new RecordId("podcast", id),
-  });
+  const out = (
+    await (
+      await database()
+    ).query("(SELECT *, image.id() OMIT audio FROM $podcast)[0]", {
+      podcast: new RecordId("podcast", id),
+    })
+  )[0];
+  if (!out) throw ClientError.notFound();
+  return out;
 }
 
 export async function getAllPodcasts() {
