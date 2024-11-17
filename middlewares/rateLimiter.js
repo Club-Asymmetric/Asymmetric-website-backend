4304fef9c04e1677759ece6382a7046fb00821eb
cosmic-striker <vishald.cs2023@citchennai.net>
2024-11-17 23:23:58 +0530

    credits updated

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -8,4 +8,11 @@ const rateLimiter = rateLimit({
   headers: true,
 });
 
-export { rateLimiter };
+const rateLimiterMain = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 25, // Limit each IP to 5 requests per windowMs
+  message: "Too many requests from this IP, please try again later.",
+  headers: true,
+});
+
+export { rateLimiter, rateLimiterMain };
